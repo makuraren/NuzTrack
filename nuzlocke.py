68d948df7e2161ea62f24726b6914bc8332f1f17
Matt.M <wildcatdud@gmail.com>
2020-11-08 02:50:39 -0500

    Update nuzlocke.py

--- nuzlocke.py
+++ nuzlocke.py
@@ -1,8 +1,17 @@
 def nuz():
-    achar = ['Kirby']
+    achar = ['Kirby', 'Mario']
     lchar = []
+    cchar = []
+    
+    checkTxt = open('char.txt', 'r')
+    items = checkTxt.readlines()
+    for line in items:
+        if line != items[len(items)-1]:
+            cchar.append(line[0:-1])
+    checkTxt.close()
+
     print()
-    achar.append(input('Please input your first fighter: '))
+    print('This program is made to simplify the tracking of characters dring the nuzlocke challenge in the world of light. If you want to add or remove a character, use the following [-/+] symbols directly infront of the name like (+Mario). Feel free to use this and distribute it to friends')
     print()
 
     while not ' ' in achar:
@@ -30,16 +39,39 @@ def nuz():
             print()
             check = input('Did you lose or gain a character: ')
             print()
-        
-            if check[0] == '+':
-                achar.append(check[1:len(check)])
-            elif check[0] == '-':
-                lchar.append(check[1:len(check)])
-                achar.remove(check[1:len(check)])
+
+            if check[1:len(check)] in cchar:            
+                if (check[0] == '+') and not(check[1:len(check)] in achar) and not(check[1:len(check)] in lchar):
+                    achar.append(check[1:len(check)])
+                elif (check[0] == '+') and (check[1:len(check)] in achar):
+                    print()
+                    print('Make sure the character doesn\'t already exist in the Available column.')
+                    print()
+                elif (check[0] == '+') and (check[1:len(check)] in lchar):
+                    print()
+                    print('This character has already been added before and placed into Lost column.')
+                    print()
+                elif (check[0] == '-') and (check[1:len(check)] in achar) and not(check[1:len(check)] in lchar):
+                    lchar.append(check[1:len(check)])
+                    achar.remove(check[1:len(check)])
+                elif (check[0] == '-') and not(check[1:len(check)] in achar):
+                    print()
+                    print('Make sure the character wanting to be removed has been added in the Available column.')
+                    print()
+                elif (check[0] == '-') and (check[1:len(check)] in lchar):
+                    print()
+                    print('This character has already been removed before and placed into Lost column.')
+                    print()
+            elif check in cchar:
+                print()
+                print('Make sure there is a +/- sign infront of the character.')
+                print()
             else:
                 print()
-                print('Make sure there is a +/- sign infront of the character')
+                print('Make sure your inputing the characters name in write')
+                print(f'For reference: {cchar}')
                 print()
+                
 
             if len(achar) == 0:
                 print()
@@ -47,7 +79,7 @@ def nuz():
                 print()
             
                 if resp2 == 'yes':
-                    achar = ['Kirby']
+                    achar = ['Kirby', 'Mario']
                     lchar = []
                     print()
                     achar.append(input('Please input your first fighter: '))
